58d651de1dfa263ecc7f3dc5f6b48e2003211a6e
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-18 14:56:15 +0100

    Create Shape.java

---
+++ com/zetcode/Shape.java
@@ -0,0 +1,117 @@
+package com.zetcode;
+
+import java.util.Random;
+
+public class Shape {
+
+    protected enum Tetrominoe { NoShape, ZShape, SShape, LineShape, 
+               TShape, SquareShape, LShape, MirroredLShape };
+
+    private Tetrominoe pieceShape;
+    private int coords[][];
+    private int[][][] coordsTable;
+
+
+    public Shape() {
+
+        coords = new int[4][2];
+        setShape(Tetrominoe.NoShape);
+    }
+
+    public void setShape(Tetrominoe shape) {
+
+         coordsTable = new int[][][] {
+            { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
+            { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
+            { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
+            { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
+            { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
+            { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
+            { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
+            { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } }
+        };
+
+        for (int i = 0; i < 4 ; i++) {
+            
+            for (int j = 0; j < 2; ++j) {
+                
+                coords[i][j] = coordsTable[shape.ordinal()][i][j];
+            }
+        }
+        
+        pieceShape = shape;
+    }
+
+    private void setX(int index, int x) { coords[index][0] = x; }
+    private void setY(int index, int y) { coords[index][1] = y; }
+    public int x(int index) { return coords[index][0]; }
+    public int y(int index) { return coords[index][1]; }
+    public Tetrominoe getShape()  { return pieceShape; }
+
+    public void setRandomShape() {
+        
+        Random r = new Random();
+        int x = Math.abs(r.nextInt()) % 7 + 1;
+        Tetrominoe[] values = Tetrominoe.values(); 
+        setShape(values[x]);
+    }
+
+    public int minX() {
+        
+      int m = coords[0][0];
+      
+      for (int i=0; i < 4; i++) {
+          
+          m = Math.min(m, coords[i][0]);
+      }
+      
+      return m;
+    }
+
+
+    public int minY() {
+        
+      int m = coords[0][1];
+      
+      for (int i=0; i < 4; i++) {
+          
+          m = Math.min(m, coords[i][1]);
+      }
+      
+      return m;
+    }
+
+    public Shape rotateLeft() {
+        
+        if (pieceShape == Tetrominoe.SquareShape)
+            return this;
+
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; ++i) {
+            
+            result.setX(i, y(i));
+            result.setY(i, -x(i));
+        }
+        
+        return result;
+    }
+
+    public Shape rotateRight() {
+        
+        if (pieceShape == Tetrominoe.SquareShape)
+            return this;
+
+        Shape result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; ++i) {
+
+            result.setX(i, -y(i));
+            result.setY(i, x(i));
+        }
+        
+        return result;
+    }
+}
