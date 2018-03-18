2021018b08eb65d06610801cdf1db74278eadaa0
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-18 14:55:56 +0100

    Create Tetris.java

---
+++ com/zetcode/Tetris.java
@@ -0,0 +1,51 @@
+package com.zetcode;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*
+Java Tetris game clone
+
+Author: Jan Bodnar
+Website: http://zetcode.com
+ */
+public class Tetris extends JFrame {
+
+    private JLabel statusbar;
+
+    public Tetris() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        Board board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(200, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public JLabel getStatusBar() {
+
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            Tetris game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
